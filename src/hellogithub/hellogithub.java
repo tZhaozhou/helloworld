b0fbb94fcd7ca22045d52a70bf52ece3ae9b766c
14187 <14187@LAPTOP-QRP0AQ5T>
2020-08-18 11:56:00 +0800

    two times

--- src/hellogithub/hellogithub.java
+++ src/hellogithub/hellogithub.java
@@ -2,6 +2,7 @@ package hellogithub;
 
 public class hellogithub {
 public static void main(String[] args) {
-	System.out.println("Hello Github");
+	System.out.println("Correct");
+//	System.out.println("Hello Github");
 }
 }
